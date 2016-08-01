4c0a6842e20ecfd8642766e7b87c6ba9c11469d0
McKenzie Weller <mckenzie.m.weller@gmail.com>
2016-07-31 21:57:04 -0600

    Moved to same folder. getGIFList moved to Buzzfeed Crawler.

---
+++ src/com/codeu/GIFMe/BuzzfeedCrawler.java
@@ -0,0 +1,193 @@
+package com.codeu.GIFMe;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Queue;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import redis.clients.jedis.Jedis;
+
+
+public class BuzzfeedCrawler {
+    // keeps track of where we started
+    private final String source;
+    
+    // the index where the results go
+    private JedisIndex index;
+    
+    // queue of URLs to be indexed
+    private Queue<String> queue = new LinkedList<String>();
+    
+    // fetcher used to get pages from Buzzfeed
+    final static BuzzfeedFetcher wf = new BuzzfeedFetcher();
+    
+    /**
+     * Constructor.
+     *
+     * @param source
+     * @param index
+     */
+    public BuzzfeedCrawler(String source, JedisIndex index) {
+        this.source = source;
+        this.index = index;
+        queue.offer(source);
+    }
+    
+    /**
+     * Returns the number of URLs in the queue.
+     *
+     * @return
+     */
+    public int queueSize() {
+        return queue.size();
+    }
+    
+    /**
+     * Gets a URL from the queue and indexes it.
+     * @param b
+     *
+     * @return Number of pages indexed.
+     * @throws IOException
+     */
+    public String crawl(boolean testing) throws IOException {
+        //didn't index a page
+        if(queue.isEmpty()){
+            return null;
+        }
+        //get the next url from the queue
+        String url = queue.poll();
+        
+        //makes sure the url hasn't been indexed
+        if(testing == false && index.isIndexed(url)){
+            return null;
+        }
+        
+        Elements paragraph;
+        if(testing){//get the contents of the url from the file
+            paragraph = wf.readBuzzfeed(url);
+        }else{//get the contents from the web
+            paragraph = wf.fetchBuzzfeed(url);
+        }
+        
+        storeGifs(url, paragraph);
+        
+        // index the page
+        index.indexPage(url, paragraph);
+        
+        //add all other internal links to the queue
+        queueInternalLinks(paragraph);
+        
+        //return the url that was indexed
+        return url;
+    }
+    
+    /**
+     * Parses paragraphs and extracts the Gifs out of the webpage
+     * Gets keywords from meta data and adds the keyword and gif to Jedis
+     *
+     * @param url
+     */
+    void storeGifs(String url, Elements paragraphs){
+        //get the keywords and store them in a temporary array
+        String keywords = paragraphs.select("meta[name=keywords]").first().attr("content");
+        keywords.replace(",", "");
+        String arr[] = keywords.split(" ");
+       
+        BufferedImage image = null;
+        
+        //for each paragraph of the webpapge
+        for(Element paragraph: paragraphs){
+            //store the gif
+            File outputFile = new File("animation.gif");
+            image = ImageIO.read(url);
+            ImageIO.write(image, "gif" outputFile);
+            
+            //add the image to each keyword
+            for(String word : arr){
+                //store the word + image
+            }
+        }
+    }
+    
+    /**
+     * Parses paragraphs and adds internal links to the queue.
+     *
+     * @param paragraphs
+     */
+    // NOTE: absence of access level modifier means package-level
+    void queueInternalLinks(Elements paragraphs) {
+        String url;
+        Elements urlLinks;
+        for (Element paragraph : paragraphs)
+        {
+            urlLinks = paragraph.select("a[href]");
+            
+            //if it's an internal link it adds it to the queue
+            for (Element link : urlLinks)
+            {
+                url = link.attr("href");
+                if (url.startsWith("/buzzfeed/"))
+                    queue.add("https://buzzfeed.com" + url);
+            }
+        }
+    }
+
+
+    public Set<String> getGifURLs(String term) {
+        Set<String> set = jedis.smembers(urlSetKey(term));
+        return set;
+    }
+
+    /**
+     * Looks up a term and returns a map from term to ArrayList of GIF URLs.
+     * 
+     * @param term
+     * @return Map from URL to count.
+     */
+    public Map<String, ArrayList> getGIFList(String term) {
+        Map<String, ArrayList> map = new HashMap<String, ArrayList>();
+        Set<String> urls = getGIFURLs(term);
+        ArrayList<String> GIFlist = new ArrayList<String>();
+        for (String url: urls) {
+            //Adds URL to ArrayList
+            GIFlist.add(url);
+        }
+        //Puts the ArrayList of GIF URLs to the keyword/term
+        map.put(term, GIFlist);
+        //TODO push map to Redis?
+        return map;
+    }
+
+    
+    public static void main(String[] args) throws IOException {
+        
+        // make a BuzzfeedCrawler
+        Jedis jedis = JedisMaker.make();
+        JedisIndex index = new JedisIndex(jedis);
+        String source = "https://buzzfeed.com";
+        BuzzfeedCrawler wc = new BuzzfeedCrawler(source, index);
+        
+        // for testing purposes, load up the queue
+        Elements paragraphs = wf.fetchBuzzfeed(source);
+        wc.queueInternalLinks(paragraphs);
+        
+        // loop until we index a new page
+        String res;
+        do {
+            res = wc.crawl(false);
+            
+            // REMOVE THIS BREAK STATEMENT WHEN crawl() IS WORKING
+            // break;
+        } while (res == null);
+        
+        //TODO get user input
+        //TODO return random URL from map
+        Map<String, ArrayList> map = index.getGifURLs("");
+        
+        }
+    }
+}
---
+++ src/com/codeu/GIFMe/JedisMaker.java
@@ -0,0 +1,111 @@
+package com.codeu.GIFMe;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import redis.clients.jedis.Jedis;
+
+
+public class JedisMaker {
+
+	/**
+	 * Make a Jedis object and authenticate it.
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	public static Jedis make() throws IOException {
+		
+		//TODO get RedisToGo URL and set up file directory
+		//String filename =
+		URL fileURL = JedisMaker.class.getClassLoader().getResource(filename);
+
+	  StringBuilder sb = new StringBuilder();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(fileURL.getFile()));
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found: " + filename);
+			return null;
+		}
+
+		while (true) {
+			String line = br.readLine();
+			if (line == null) break;
+			sb.append(line);
+		}
+		br.close();
+
+		URI uri;
+		try {
+			uri = new URI(sb.toString());
+		} catch (URISyntaxException e) {
+			System.out.println("Reading file: " + filename);
+			System.out.println("It looks like this file does not contain a valid URI.");
+			return null;
+		}
+		String host = uri.getHost();
+		int port = uri.getPort();
+
+		String[] array = uri.getAuthority().split("[:@]");
+		String auth = array[1];
+
+		//Here's an older version that read the auth code from an environment variable.
+		//String host = "dory.redistogo.com";
+		//int port = 10534;
+		//String auth = System.getenv("REDISTOGO_AUTH");
+
+		Jedis jedis = new Jedis(host, port);
+
+		try {
+			jedis.auth(auth);
+		} catch (Exception e) {
+			System.out.println("Trying to connect to " + host);
+			System.out.println("on port " + port);
+			System.out.println("with authcode " + auth);
+			System.out.println("Got exception " + e);
+			printInstructions();
+			return null;
+		}
+		return jedis;
+	}
+
+
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+		Jedis jedis = make();
+
+		// String
+		jedis.set("mykey", "myvalue");
+		String value = jedis.get("mykey");
+	    System.out.println("Got value: " + value);
+
+	    // Set
+	    jedis.sadd("myset", "element1", "element2", "element3");
+	    System.out.println("element2 is member: " + jedis.sismember("myset", "element2"));
+
+	    // List
+	    jedis.rpush("mylist", "element1", "element2", "element3");
+	    System.out.println("element at index 1: " + jedis.lindex("mylist", 1));
+
+	    // Hash
+	    jedis.hset("myhash", "word1", Integer.toString(2));
+	    jedis.hincrBy("myhash", "word2", 1);
+	    System.out.println("frequency of word1: " + jedis.hget("myhash", "word1"));
+	    System.out.println("frequency of word2: " + jedis.hget("myhash", "word2"));
+
+	    jedis.close();
+	}
+}
