fa8780d3ffc30af93320dbd8c6d13ca801efd29a
McKenzie Weller <mckenzie.m.weller@gmail.com>
2016-07-31 22:41:36 -0600

    redis instance added

--- src/com/codeu/GIFMe/JedisMaker.java
+++ src/com/codeu/GIFMe/JedisMaker.java
@@ -24,7 +24,7 @@ public class JedisMaker {
 	public static Jedis make() throws IOException {
 		
 		//TODO get RedisToGo URL and set up file directory
-		String filename = redis_url.txt;
+		String filename = "redis_url.txt";
 		URL fileURL = JedisMaker.class.getClassLoader().getResource(filename);
 
 	  StringBuilder sb = new StringBuilder();
